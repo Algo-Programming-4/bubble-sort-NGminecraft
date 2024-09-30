d0bb41f1b8802b3a8fa768c9a7ba92ca79d468f2
NGminecraft <grahanic001@alpinesd.org>
2024-09-30 13:35:46 -0600

    Adding funcionality for selection sort

--- sorting.py
+++ sorting.py
@@ -5,8 +5,19 @@ def bubble(lst):
                 lst[i], lst[i+1]= lst[i+1], v
     return lst
 
+def selection(lst):
+    for full_index in range(len(lst)-1):
+        index_smallest = full_index+1
+        for index, value in enumerate(lst[full_index:]):
+            if value < lst[index_smallest]:
+                index_smallest = index+full_index
+        lst[index_smallest], lst[full_index] = lst[full_index], lst[index_smallest]
+    return lst
+                
+                
+        
 
 if __name__ == "__main__":
     nums = [1, 3, 8, 19, 2, 7, -8, 27]
-    a = bubble(nums)
+    a = selection(nums)
     print(a)
--- unit_sorted_test.py
+++ unit_sorted_test.py
@@ -3,12 +3,21 @@ import random
 import unittest
 
 
-class Test(unittest.TestCase):
+class sorting_test(unittest.TestCase):
     def test_sorted(self):
         for _ in range(10):
             nums = [random.randint(1,100) for x in range(random.randint(1,100))]
             result =  sorting.bubble(nums)
             self.assertEqual(result, sorted(nums))
+            
+    def test_selection(self):
+        for _ in range(10):
+            nums = [random.randint(1,100) for x in range(random.randint(1,100))]
+            result =  sorting.selection(nums)
+            self.assertEqual(result, sorted(nums))
       
 if __name__ == "__main__":
-    Test().test_sorted()
+    test = sorting_test()
+    test.test_sorted()
+    test.test_selection()
+    
